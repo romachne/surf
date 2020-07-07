4b7654c5df4718caa0430a6000ab618ac7eed977
Roma <romachne@gmail.com>
2020-07-07 22:22:42 +0500

    крч заново все

--- config/params.php
+++
@@ -1,8 +0,0 @@
-<?php 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-define("DS", DIRECTORY_SEPARATOR);
-define("ROOT", __DIR__.'/');
-define("SITE_PATH", "http://localhost/surf/");
-?>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'includes/header.php';
-
-    if (isset($_SESSION['admin_session']) )
-    {
-        session::destroy('admin_session');
-        $commons->redirectTo(SITE_PATH.'index.php');
-    }
-?>
