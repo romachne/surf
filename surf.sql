08e82fe1db8636ed92dc702d721cc5278545c7c1
Roma <romachne@gmail.com>
2020-07-11 20:17:27 +0500

    bd

--- surf.sql
+++ surf.sql
@@ -1,68 +1,188 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 12, 2016 at 05:46 PM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
+-- Хост: 127.0.0.1
+-- Время создания: Июл 11 2020 г., 17:17
+-- Версия сервера: 10.4.13-MariaDB
+-- Версия PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `surf`
+-- База данных: `surf`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admins`
+-- Структура таблицы `admins`
 --
 
-CREATE TABLE IF NOT EXISTS `admins` (
-  `idadmins` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admins` (
+  `idadmins` int(11) NOT NULL,
   `username` varchar(122) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  PRIMARY KEY (`idadmins`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+  `password` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `admins`
+-- Дамп данных таблицы `admins`
 --
 
 INSERT INTO `admins` (`idadmins`, `username`, `password`) VALUES
-(1, 'roma', 'root12345');
+(7, 'roman', '$2y$10$q/SMBWxTZwZFaMRCf5gd4edMn71L7C50sfclbBPFv7um3dg3g9slS');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Структура таблицы `kitesurfing`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(255) NOT NULL,
-  `product_price` float NOT NULL,
-  `product_expires_on` date NOT NULL,
-  `product_description` text NOT NULL,
-  `created_on` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+CREATE TABLE `kitesurfing` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `head2` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `products`
+-- Дамп данных таблицы `kitesurfing`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_expires_on`, `product_description`, `created_on`) VALUES
-(7, 'Autralian Mangoes', 42.99, '2016-04-30', 'Installing Slim 3 through composer will install all its dependencies, when doing a PHP file count you will notice we have doubled in file count. This is a given with the amount of flexibility we now have. Most developers might not see any benefit in this as they will likely just work with what is provided, but if at any point you should hit a limitation in any working part of the framework, you can easily swap it out without a fuss.', '1457798455');
+INSERT INTO `kitesurfing` (`id`, `headline`, `head2`, `description`) VALUES
+(1, '13', '13', '31dasd');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sapsurfing`
+--
+
+CREATE TABLE `sapsurfing` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `head2` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `sapsurfing`
+--
+
+INSERT INTO `sapsurfing` (`id`, `headline`, `head2`, `description`) VALUES
+(1, '312asdasdasd', '312', '123asdas');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `surfing`
+--
+
+CREATE TABLE `surfing` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `head2` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `surfing`
+--
+
+INSERT INTO `surfing` (`id`, `headline`, `head2`, `description`) VALUES
+(1, '312asdasd', '312', '123'),
+(2, 'афыафы', 'афафы', 'афафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафыафафы');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `wakeboarding`
+--
+
+CREATE TABLE `wakeboarding` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `head2` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`idadmins`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Индексы таблицы `kitesurfing`
+--
+ALTER TABLE `kitesurfing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `sapsurfing`
+--
+ALTER TABLE `sapsurfing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `surfing`
+--
+ALTER TABLE `surfing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `wakeboarding`
+--
+ALTER TABLE `wakeboarding`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `idadmins` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `kitesurfing`
+--
+ALTER TABLE `kitesurfing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `sapsurfing`
+--
+ALTER TABLE `sapsurfing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `surfing`
+--
+ALTER TABLE `surfing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `wakeboarding`
+--
+ALTER TABLE `wakeboarding`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
